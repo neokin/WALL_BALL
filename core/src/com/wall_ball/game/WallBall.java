9356db35609ff8b0f91a49a27b667b89646f6660
Kirill <mazkirand@gmail.com>
2016-12-10 23:26:01 +0300

    Add total score string, rings and sound

--- core/src/com/wall_ball/game/WallBall.java
+++ core/src/com/wall_ball/game/WallBall.java
@@ -11,6 +11,7 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.scenes.scene2d.ui.Button;
@@ -23,31 +24,33 @@ public class WallBall implements Screen {
     SpriteBatch batch;
     OrthographicCamera camera;
 
-    static float x0;
-    //int counter;
-    static float y0;
+
+    int counter = 0;
+
     Texture ball_img;
     Texture lcomp;
-    boolean end;
-
+    Texture ring_img;
 
     Texture longhorizontal_img, longvertical_img, shorthorizontal_img, red_img;
     Sound snd;
     Music msc;
     Vector3 newPos = new Vector3();
-    Rectangle ball_rect, longhorizontal_r, longvertical_1, longvertical_2, shorthorizontal_1, shorthorizontal_2, red_r;
+
+    Rectangle ball_rect, black_r, ring_r;
 
     public WallBall(final Ball game) {
+        //counter = 0;
         this.game = game;
         batch = new SpriteBatch();
 
-        snd = Gdx.audio.newSound(Gdx.files.internal("congrat.mp3"));
+        snd = Gdx.audio.newSound(Gdx.files.internal("kick.wav"));
         //msc = Gdx.audio.newMusic(Gdx.files.internal("main.mp3"));
         lcomp = new Texture("lcomp.png");
+        ring_img = new Texture("ring.png");
         ball_img = new Texture((Gdx.files.internal("ball.png")));
-        longhorizontal_img = new Texture(Gdx.files.internal("longhorizontal.png"));
-        longvertical_img = new Texture(Gdx.files.internal("longvertical.png"));
-        shorthorizontal_img = new Texture(Gdx.files.internal("shorthorizontal.png"));
+        longhorizontal_img = new Texture(Gdx.files.internal("lh.png"));
+        longvertical_img = new Texture(Gdx.files.internal("lv.png"));
+        shorthorizontal_img = new Texture(Gdx.files.internal("sh.png"));
         red_img = new Texture("red.png");
 
         //msc.setLooping(true);
@@ -55,48 +58,23 @@ public class WallBall implements Screen {
 
         camera = new OrthographicCamera();
         camera.setToOrtho(false, 800, 480);
-        batch = new SpriteBatch();
         ball_rect = new Rectangle();
         ball_rect.x = 400;
         ball_rect.y = 240;
         ball_rect.width = 60;
         ball_rect.height = 60;
 
-        longhorizontal_r = new Rectangle();
-        longhorizontal_r.x = 40;
-        longhorizontal_r.y = 0;
-        longhorizontal_r.width = 720;
-        longhorizontal_r.height = 40;
+        black_r = new Rectangle();
+        black_r.x = 410 - 60 / 2;
+        black_r.y = 440;
+        black_r.width = 60;
+        black_r.height = 40;
 
-        shorthorizontal_1 = new Rectangle();
-        shorthorizontal_1.x = 40;
-        shorthorizontal_1.y = 440;
-        shorthorizontal_1.width = 320;
-        shorthorizontal_1.height = 40;
-
-        shorthorizontal_2 = new Rectangle();
-        shorthorizontal_2.x = 440;
-        shorthorizontal_2.y = 440;
-        shorthorizontal_2.width = 320;
-        shorthorizontal_2.height = 40;
-
-        longvertical_1 = new Rectangle();
-        longvertical_1.x = 0;
-        longvertical_1.y = 0;
-        longvertical_1.width = 40;
-        longvertical_1.height = 480;
-
-        longvertical_2 = new Rectangle();
-        longvertical_2.x = 760;
-        longvertical_2.y = 0;
-        longvertical_2.width = 40;
-        longvertical_2.height = 480;
-
-        red_r = new Rectangle();
-        red_r.x = 410 - 60 / 2;
-        red_r.y = 440;
-        red_r.width = 60;
-        red_r.height = 40;
+        ring_r = new Rectangle();
+        ring_r.width = 40;
+        ring_r.height = 40;
+        ring_r.x = MathUtils.random(80, 700);
+        ring_r.y = MathUtils.random(80, 400);
     }
 
     @Override
@@ -109,17 +87,21 @@ public class WallBall implements Screen {
         Gdx.gl.glClearColor(0, 0, 0.3f, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
+
         camera.update();
         batch.setProjectionMatrix(camera.combined);
         batch.begin();
+
         batch.draw(ball_img, ball_rect.x, ball_rect.y);
-        batch.draw(longhorizontal_img, longhorizontal_r.x, longhorizontal_r.y);
-        batch.draw(shorthorizontal_img, shorthorizontal_1.x, shorthorizontal_1.y);
-        batch.draw(shorthorizontal_img, shorthorizontal_2.x, shorthorizontal_2.y);
-        batch.draw(longvertical_img, longvertical_1.x, longvertical_1.y);
-        batch.draw(longvertical_img, longvertical_2.x, longvertical_2.y);
-        batch.draw(longvertical_img, longvertical_2.x, longvertical_2.y);
-        batch.draw(red_img, red_r.x, red_r.y);
+
+        batch.draw(shorthorizontal_img, 38, 440);
+        batch.draw(shorthorizontal_img, 420, 440);
+        batch.draw(longvertical_img, 0, 0);
+        batch.draw(longvertical_img, 760, 0);
+        batch.draw(longhorizontal_img, 40, -5);
+        batch.draw(red_img, black_r.x, black_r.y);
+        batch.draw(ring_img, ring_r.x, ring_r.y);
+        game.font.draw(batch, "Total score: " + counter, 40, 440);
         batch.end();
 
 
@@ -128,30 +110,38 @@ public class WallBall implements Screen {
         float y = Gdx.input.getRoll();
 
         if (x < 0) {
-            ball_rect.x = (ball_rect.x + 3);
-        } else ball_rect.x = (ball_rect.x - 3);
+            ball_rect.x = (ball_rect.x + 2.8f);
+        } else ball_rect.x = (ball_rect.x - 2.8f);
         if (y > -40) {
-            ball_rect.y = (ball_rect.y + 3);
-        } else ball_rect.y = (ball_rect.y - 3);
+            ball_rect.y = (ball_rect.y + 2.8f);
+        } else ball_rect.y = (ball_rect.y - 2.8f);
 
-        newPos.set(ball_rect.x, ball_rect.y, 0);
-        camera.unproject(newPos);
+        //newPos.set(ball_rect.x, ball_rect.y, 0);
+        //camera.unproject(newPos);
 
 
         if (ball_rect.x < 40) ball_rect.x = 40;
-        if (ball_rect.x > 800 - 60 - 40) ball_rect.x = 800 - 60 - 40;
+        if (ball_rect.x > 700) ball_rect.x = 700;
         if (ball_rect.y < 40) ball_rect.y = 40;
-        if (ball_rect.x < red_r.x - red_r.width / 2 || ball_rect.x > red_r.x + red_r.width / 2) {
-            if (ball_rect.y > 480 - 60 - 40) ball_rect.y = 480 - 60 - 40;
-        } else if (ball_rect.y > 480 - 60 - 40) {
+        if (ball_rect.x < black_r.x - black_r.width / 2 || ball_rect.x > black_r.x + black_r.width / 2) {
+            if (ball_rect.y > 384) ball_rect.y = 384;
+        } else if (ball_rect.y > 384) {
             //snd.play();
 
             camera.update();
-           // batch.begin();
+            // batch.begin();
             //batch.draw(lcomp, 0, 0);
             //batch.end();
 
 
+
+        }
+        if(ball_rect.overlaps(ring_r)) {
+            snd.play();
+            ring_r.x = MathUtils.random(80, 700);
+            ring_r.y = MathUtils.random(80, 400);
+            counter++;
+            snd.play();
 
         }
         //counter = counter + 1;
@@ -183,6 +173,12 @@ public class WallBall implements Screen {
         batch.dispose();
 
         snd.dispose();
+
+        red_img.dispose();
+        shorthorizontal_img.dispose();
+        longvertical_img.dispose();
+        longhorizontal_img.dispose();
+        ball_img.dispose();
 
 
     }
